42e6b0a8ccc8e670048a0671f15ba24d1bda0052
Ceyene <cyn.jaz.romero@gmail.com>
2020-10-19 22:44:22 -0300

    completas configuraciones

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+//creamos componente funcional
+const HelloWorld = ()=> (
+    <h1>Hola Mundo</h1>
+)
+
+
+export default HelloWorld;
