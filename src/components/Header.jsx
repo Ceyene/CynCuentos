858edaa3cbb59dc7ea8c699205bbf4062a6d7aec
Ceyene <cyn.jaz.romero@gmail.com>
2020-10-24 16:10:56 -0300

    agregando componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//COMPONENTE PRESENTACIONAL
+//(sin lógica, solo envía una sección HTML a nuestra construcción)
+
+const Header = () => (
+    //return explícito
+    <header className="header">
+        <img className="header__img" src="../assets/images/logoceyeneseries.png" alt="Ceyene Series Logo" />
+        <div className="header__menu">
+            <div className="header__menu--profile">
+                <img src="../assets/images/usuario.png" alt="Usuario" />
+                <p>Perfil</p>
+            </div>
+            <ul>
+                <li>
+                    <a href="login.html">Cuenta</a>
+                </li>
+                <li>
+                    <a href="login.html">Cerrar Sesión</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+);
+
+export default Header;
