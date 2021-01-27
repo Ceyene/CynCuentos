fa7d29eca20418f7f14e1f73a9c22265c5664078
Cyn Romero <cyn.jaz.romero@gmail.com>
2021-01-27 16:42:49 -0300

    añadida la página Not found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/styles/components/NotFound.scss";
+
+const NotFound = () => (
+  <section className="not-found__container">
+    <section className="not-found">
+      <h1>404</h1>
+      <p>Página no encontrada</p>
+      <a href="index.html">Volver a inicio</a>
+    </section>
+  </section>
+);
+
+export default NotFound;
